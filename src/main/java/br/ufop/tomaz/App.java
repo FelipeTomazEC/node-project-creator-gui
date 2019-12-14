be93f8b69a5f88781cf50a57a2ffca6fe745c762
FelipeTomazEC <felipetomaz2206@gmail.com>
2019-12-14 16:00:33 -0300

    Created launcher.

--- src/main/java/br/ufop/tomaz/App.java
+++ src/main/java/br/ufop/tomaz/App.java
@@ -1,4 +1,27 @@
 package br.ufop.tomaz;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private static Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        AnchorPane pane = new AnchorPane();
+        Label label = new Label("NODE PROJECT CREATOR.");
+        pane.getChildren().add(label);
+        Scene scene = new Scene(pane, 400, 400);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/br/ufop/tomaz/Launcher.java
+++ src/main/java/br/ufop/tomaz/Launcher.java
@@ -1,4 +1,7 @@
 package br.ufop.tomaz;
 
 public class Launcher {
+    public static void main(String[] args) {
+        App.main(args);
+    }
 }
